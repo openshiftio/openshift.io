675b5bcf799442fdc70c4c27bca30c01b91f324e
Pete Muir <pmuir@bleepbleep.org.uk>
2017-03-01 14:07:51 -0500

    feat: use sass, finish logged in menu

--- app/index.ts
+++ app/index.ts
@@ -2,6 +2,9 @@ import 'core-js/es6/string';
 
 import * as $ from 'jquery';
 import * as URI from 'urijs';
+import '../assets/stylesheets/variables.scss';
+import '../assets/stylesheets/custom.scss';
+import './header.scss';
 
 export class ApiLocator {
 
@@ -34,34 +37,95 @@ export class ApiLocator {
 
 }
 
+export class Token {
+  'access_token' : string;
+  'expires_in' : number;
+  'refresh_expires_in' : number;
+  'refresh_token' : string;
+  'token_type' : string;
+}
+
 export class Auth {
 
   readonly APP_PATH = '/home';
   private api : ApiLocator = new ApiLocator();
   private apiUrl : string;
 
+  private refreshInterval : number;
+  private clearTimeoutId : any;
+
+  private loggedIn : boolean;
+
+  private authToken : string;
+
   constructor() {
     console.log('Auth API URL:', this.apiUrl);
     this.apiUrl = this
       .api
       .buildApiUrl(AUTH_API_URL, 'api', 'api');
+    this.authToken = localStorage.getItem('auth_token');
   }
 
   login() {
     window.location.href = this.apiUrl + '/login/authorize';
   }
 
+  logout() {
+    localStorage.removeItem('auth_token');
+    localStorage.removeItem('refresh_token');
+    this.authToken = null;
+    clearTimeout(this.clearTimeoutId);
+    this.refreshInterval = null;
+    this.loggedIn = false;
+    this.updateUserMenu();
+  }
+
+  setupRefreshTimer(refreshInSeconds : number) {
+    if (!this.clearTimeoutId) {
+      let refreshInMs = Math.round(refreshInSeconds * .9) * 1000;
+      console.log('Refreshing token in: ' + refreshInMs + ' milliseconds.');
+      this.refreshInterval = refreshInMs;
+      this.clearTimeoutId = setTimeout(() => this.refreshToken(), refreshInMs);
+    }
+  }
+
+  refreshToken() {
+    if (this.loggedIn) {
+      this.clearTimeoutId = null;
+      let refreshTokenUrl = this.apiUrl + '';
+      let refreshToken = localStorage.getItem('refresh_token');
+      let body = JSON.stringify({"refresh_token": refreshToken});
+      $.ajax({
+        url: this.apiUrl + 'login/refresh',
+        headers: {
+          "Authorization": "Bearer " + this.authToken,
+          'Content-Type': "application/json"
+        },
+        method: 'GET',
+        dataType: 'json',
+        success: response => {
+          let responseJson = response.json();
+          let token = this.processTokenResponse(responseJson.token);
+          this.setupRefreshTimer(token.expires_in);
+          console.log('token refreshed at:' + Date.now());
+
+        }
+      });
+    }
+  }
+
   handleLogin(url : uri.URI) {
     let query = url.query(true)as any;
     if (url.hasQuery('token')) {
-      let token = query['token'];
+      this.loggedIn = true;
+      this.authToken = query['token'];
       let redir = new URI('/home')
-        .addQuery({token: token})
+        .addQuery({token: this.authToken})
         .toString();
       console.log('Authentication succeeded, redirecting to', redir);
       window
         .localStorage
-        .setItem('authToken', token);
+        .setItem('auth_token', this.authToken);
       window.location.href = redir;
     }
   }
@@ -79,20 +143,16 @@ export class Auth {
   }
 
   updateUserMenu() {
-    let token = window
-      .localStorage
-      .getItem('authToken');
-    if (token) {
+    if (this.authToken) {
       $.ajax({
         url: this.apiUrl + 'user',
         headers: {
-          "Authorization": "Bearer " + token,
+          "Authorization": "Bearer " + this.authToken,
           'Content-Type': "application/json"
         },
         method: 'GET',
         dataType: 'json',
         success: response => {
-          console.log(response);
           let user = response.data;
           if (user.attributes.imageURL) {
             $("#userimage")
@@ -102,12 +162,24 @@ export class Auth {
             $("#nouserimage").removeClass("hidden");
           }
           $("#name").html(user.attributes.fullName);
+          $("#profilelink").attr("href", "/" + user.attributes.username);
+          $("#settingslink").attr("href", "/" + user.attributes.username + "/_settings");
           $("#loggedout").hide();
           $("#loggedin").removeClass('hidden');
         }
       });
+    } else {
+      $("#loggedout").show();
+      $("#loggedin").hide();
     }
+  }
 
+  processTokenResponse(response : any) : Token {
+    let token = response as Token;
+    this.authToken = token.access_token;
+    localStorage.setItem('auth_token', this.authToken);
+    localStorage.setItem('refresh_token', token.refresh_token);
+    return token;
   }
 }
 
@@ -137,6 +209,10 @@ $(document)
 
     $("a#login").click(function () {
       auth.login();
+    });
+
+    $("a#logout").click(function () {
+      auth.logout();
     });
 
   });
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,37 @@
+const path = require('path');
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const DefinePlugin = require('webpack/lib/DefinePlugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+// Helper functions
+var ROOT = path.resolve(__dirname, '.');
+
+const sassModules = [
+  {
+    name: 'bootstrap'
+  }, {
+    name: 'font-awesome',
+    module: 'font-awesome',
+    path: 'font-awesome',
+    sass: 'scss'
+  }, {
+    name: 'patternfly',
+    module: 'patternfly-sass-with-css'
+  }
+];
+
+sassModules.forEach(val => {
+  val.module = val.module || val.name + '-sass';
+  val.path = val.path || path.join(val.module, 'assets');
+  val.modulePath = val.modulePath || path.join('node_modules', val.path);
+  val.sass = val.sass || path.join('stylesheets');
+  val.sassPath = path.join(ROOT, val.modulePath, val.sass);
+});
+
+const extractSass = new ExtractTextPlugin({
+    filename: "[name].[contenthash].css",
+    disable: process.env.NODE_ENV === "development"
+});
 
 module.exports = {
   entry: './app/index.ts',
@@ -14,15 +46,29 @@ module.exports = {
         test: /\.tsx?$/,
         loader: 'ts-loader',
         exclude: /node_modules/
+      }, {
+        test: /\.scss$/,
+        use: extractSass.extract([
+          {
+            loader: 'css-loader'
+          }, {
+            loader: 'sass-loader',
+            options: {
+              includePaths: sassModules.map(val => {
+                return val.sassPath;
+              })
+            }
+          }
+        ])
       }
-    ],
+    ]
   },
   resolve: {
     extensions: [".tsx", ".ts", ".js"]
   },
   plugins: [
-      
-      /*
+
+    /*
        * Plugin: HtmlWebpackPlugin
        * Description: Simplifies creation of HTML files to serve your webpack bundles.
        * This is especially useful for webpack bundles that include a hash in the filename
@@ -30,29 +76,26 @@ module.exports = {
        *
        * See: https://github.com/ampedandwired/html-webpack-plugin
        */
-      new HtmlWebpackPlugin({ 
-        template: 'app/index.html', 
-        chunksSortMode: 'dependency',
+    new HtmlWebpackPlugin({template: 'app/index.html', chunksSortMode: 'dependency'}),
+    new DefinePlugin({
+      AUTH_API_URL: JSON.stringify(process.env.FABRIC8_WIT_API_URL)
+    }),
+    extractSass
 
-      }),
-      new DefinePlugin({
-        AUTH_API_URL: JSON.stringify(process.env.FABRIC8_WIT_API_URL)
-      })
-      
-    ],
-    
-    /*
+  ],
+
+  /*
      * Include polyfills or mocks for various node stuff
      * Description: Node configuration
      *
      * See: https://webpack.github.io/docs/configuration.html#node
      */
-    node: {
-      global: true,
-      crypto: 'empty',
-      process: true,
-      module: false,
-      clearImmediate: false,
-      setImmediate: false
-    }
+  node: {
+    global: true,
+    crypto: 'empty',
+    process: true,
+    module: false,
+    clearImmediate: false,
+    setImmediate: false
+  }
 };
